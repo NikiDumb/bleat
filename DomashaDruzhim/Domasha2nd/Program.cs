912919d2ec0fcfc199effe2a7f8581a2d67c2cbd
NikiDumb <70842168+NikiDumb@users.noreply.github.com>
2024-06-21 19:25:48 +0300

    2 lesson

---
+++ DomashaDruzhim/Domasha2nd/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Domasha2nd
+{
+    internal class Program
+    {
+
+        static void TicketOffice(string productName, double price, double count, bool onSale, int count4sale=5, double totalSale = 0)
+        {
+            if (count >= count4sale) totalSale += 0.1;
+            if (onSale == true) totalSale += 0.15;
+
+            double totalPrice = price * count - price * totalSale * count;
+
+            Console.WriteLine($"ИП Максем\r\n{productName} {count}шт цена за шт {price}руб\r\nИтого: {totalPrice}руб" );
+
+        }
+        static void RestoTables(int clientsCount)
+        {
+            int varTable = 0;
+
+            if (clientsCount > 0 & clientsCount <= 2) varTable = 1;
+            else if (clientsCount > 2 & clientsCount <= 4) varTable = 2;
+            else if (clientsCount > 4 & clientsCount <= 8) varTable = 3;
+
+            Console.WriteLine($"Тип вашего посадочного места: {varTable}. (если там нолик петляйте отсюда, мы словарь не подгрузили просто)");
+        }
+
+        static void SalesManager(string name, int age, int ordersCount, double totalExpense, int age4sale = 60, double expense4sale = 5000, double orders4sale = 4 )
+        {
+            bool sale = false;
+
+            if (age > 60 || ordersCount >= orders4sale & totalExpense >= expense4sale) sale = true;
+
+            Console.WriteLine($"Получит ли {name} скидку?\r\n{sale}");
+        }
+        static void CourierValidator(double length, double width, double heigth, double distance, double validateGabaritesSum = 150, double maxMeasure = 100, double minDistance = 3, double maxDistance = 10 )
+        {
+            if (length+width+heigth>validateGabaritesSum)
+            {
+                Console.WriteLine("Не прохожу по габаритам");
+                return;
+            }
+            if (length>maxMeasure || width > maxMeasure || heigth > maxMeasure)
+            {
+                Console.WriteLine("Слишком большой у тебя он");
+                return;
+            }
+            if (distance>maxDistance || distance<minDistance)
+            {
+                Console.WriteLine("Плохо с расстоянием");
+                return;
+            }
+
+            Console.WriteLine("Все ок)");
+        }
+        static void Main(string[] args)
+        {
+            TicketOffice("Ипотека", 100, 5, true);
+            RestoTables(9);
+            SalesManager("Алиса", 16, 4, 5000);
+            CourierValidator(10, 10, 10, 5); ;
+        }
+    }
+}
---
+++ DomashaDruzhim/DomashaDruzhim/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace DomashaDruzhim
+{
+    internal class Program
+    {
+        static void AuchanSalary (double salesCost, double salesPlan, double planFactor = 0.05, double overPlanFactor = 0.1)
+            {
+                double sellerSalary = salesCost* planFactor;
+                if (salesCost >= salesPlan)
+                {
+                    sellerSalary = salesPlan* planFactor + (salesCost-salesPlan)* overPlanFactor;
+                }
+
+                Console.WriteLine($"Зарплата сотрудника: {sellerSalary}руб");
+            }
+
+        static void UMenyaTeperIpotekaMaksem(double money, double smallIncome = 1.08, double middleIncome = 1.095, double largeIncome = 1.1)
+            {
+                double[] finalMoney = [money, money, money];
+
+                for (int i = 0; i<18; i++)
+                {
+                    if (i<9) finalMoney[0]*=smallIncome;
+                    if (i<12) finalMoney[1]*=middleIncome;
+                    if (i<18) finalMoney[2]*=largeIncome;
+                }
+
+                Console.WriteLine("Прибыль вашего вклада:");
+                Console.WriteLine($"через 9 месяцев: {finalMoney[0]}руб");
+                Console.WriteLine($"через 12 месяцев: {finalMoney[1]}руб");
+                Console.WriteLine($"через 18 месяцев: {finalMoney[2]}руб");
+            }
+        static void BestBanking(double money, int period, double income)
+        {
+            double totalMoney = money * Math.Pow(income, period);
+
+            Console.WriteLine($"Денежки через {period} месяцев: {totalMoney}руб");
+        }
+        static void CourierPrice(double distance, int fixedPrice = 150, int distancePrice = 70)
+            {
+                int totalCost = fixedPrice + (int)distance * distancePrice;
+
+                Console.WriteLine($"Итоговая стоимость доставки: {totalCost}руб");   
+            }
+        static void Main(string[] args)
+        { 
+            CourierPrice(8);
+            AuchanSalary(180000, 140000);
+            UMenyaTeperIpotekaMaksem(1000000);
+            BestBanking(1000000, 9, 1.08);
+        }
+    }
+}
